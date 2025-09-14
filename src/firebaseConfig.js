9e2583f8c03895a86419d618fa3e9f40a68a9988
Giannfranco Crovetto <gcrovetto2021@fau.edu>
2025-09-14 11:43:43 -0400

    Installed Firebase and added a FirebaseConfig to set it up

---
+++ src/firebaseConfig.js
@@ -0,0 +1,23 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "REMOVED_SECRET",
+  authDomain: "acm-application-5eb4e.firebaseapp.com",
+  projectId: "acm-application-5eb4e",
+  storageBucket: "acm-application-5eb4e.firebasestorage.app",
+  messagingSenderId: "1069610101597",
+  appId: "1:1069610101597:web:4a1dbd9a4b01bd66cfd6b4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+export const auth = getAuth(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+export const db = getFirestore(app);
+
+export default app;
